77042decfcbf4ebd92c60f5aaf11e1fb91df7582
IrinaSemGB <irina.semyachkina@mail.ru>
2021-08-26 16:45:07 +0300

    Weather App created

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,35 @@
+import 'package:flutter/cupertino.dart';
+
+import 'location.dart';
+import 'networking.dart';
+
+
+const apiKey = '92cabe9523da26194b02974bfcd50b7e';
+const openWeatherMapURL = 'http://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  
+  Future<dynamic> getCityWeather(String cityName) async {
+
+    NetworkHelper networkHelper = NetworkHelper(
+      Uri.parse('$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric'),
+    );
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentPosition();
+
+    var url = Uri.parse(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+    NetworkHelper networkHelper = NetworkHelper(url);
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
